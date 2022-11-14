7e932315840f296b22f29a5c7411cbcf10f5229a
Irabiel <irabiel@yahoo.com>
2022-11-13 23:21:43 -0800

    created the class for the inverese problem

--- DAE_Inverse_Problem_cpp/Inverse_Problem_Fns.cpp
+++ DAE_Inverse_Problem_cpp/Inverse_Problem_Fns.cpp
@@ -4,19 +4,29 @@
 
 #include "Inverse_Problem_Fns.h"
 #include "Needed_functions.h"
+#include "math.h"
 
-void Forward_solver(std::vector<double> & x, std::vector<double> & y, double m) {
+#include <stdlib.h>     /* srand, rand */
 
+Inv_Prb::Inv_Prb() {
+
+}
+
+void Inv_Prb::Forward_solver(std::vector<double> & x, std::vector<double> & y, double m) {
+    double k1;
+    double k2;
+    double k3;
+    double k4;
     for (int i = 0; i < x.size(); i++) {
 
         int iter = 0;
         int max_iter = 10;
-        double s = 0;
+        double s = 0.;
         double yst = y[i];
         while (iter < max_iter){
             s = -g(x[i], yst, m) / gy(x[i], yst, m);
             yst += s;
-            if abs(s) < epislon_DAE{
+            if (abs(s) < epislon_DAE){
                 y[i] = yst;
                 break;
             }
@@ -33,15 +43,139 @@ void Forward_solver(std::vector<double> & x, std::vector<double> & y, double m) 
     }
 };
 
-void Adjoint_solver(std::vector<double> & x, std::vector<double> & y, double m
-                    , std::vector<double> & la, std::vector<double> & mu){
+void Inv_Prb::Adjoint_solver(std::vector<double> & x, std::vector<double> & y, double m
+                    , std::vector<double> & la, std::vector<double> & mu, std::vector<double> & d){
     la[la.size()-1] = 0;
     mu[mu.size()-1] = 0;
     // Backwards Euler
-    for (int t = x.size() - 2 ; t >= 0; t--){
-        la[t] = (1. + dt * fx(x[t + 1], y[t + 1], m)) * la[t + 1] + dt * ((x[t + 1] - d[t + 1])
-                                                                         + mu[t + 1] * gx(x[t + 1], y[t + 1], m));
-        mu[t] = -la[t] * (fy(x[t], y[t], m) / gy(x[t], y[t], m));
+    for (int t = x.size() - 1 ; t >= 0; t--){
+        la[t-1] = (1 + dt * fx(x[t], y[t],m) ) * la[t] + dt * ( (x[t] - d[t])
+                                                                + mu[t]* gx(x[t],y[t],m) );
+        mu[t-1] = - la[t-1] * (fy(x[t-1],y[t-1],m) / gy(x[t-1],y[t-1],m));
     }
 };
 
+void Inv_Prb::Grad_eval(std::vector<double> & x, std::vector<double> & y, std::vector<double> & la, std::vector<double> & mu, double m, double gnorm
+        , double grad){
+    grad = 0.;
+    for (int t = 0; t < x.size(); t++)
+        grad += la[t] * fm(x[t],y[t],m) + mu[t] * gm(x[t],y[t],m);
+
+    grad *= dt;
+
+    gnorm = abs(grad);
+}
+
+double Inv_Prb::cost(std::vector<double> & x, std::vector<double> & d){
+    double cost = 0.;
+    for (int t = 0; t < x.size(); t++)
+        cost += (x[t] - d[t]) * (x[t] - d[t]);
+
+    cost *= dt/2;
+    return cost;
+}
+
+void Inv_Prb::Inc_state(std::vector<double> & x, std::vector<double> & y, double m
+        , std::vector<double> & xh, std::vector<double> & yh, double mh){
+    double temp_sum = 0.;
+    for (int t = 0; t < x.size()-1; t++) {
+
+        temp_sum = 1. - dt * fx(x[t + 1], y[t + 1], m) + dt * (gx(x[t + 1], y[t + 1], m) * fy(x[t + 1], y[t + 1], m)
+                                                              / gy(x[t + 1], y[t + 1], m));
+
+        xh[t + 1] = (1. / temp_sum) * (xh[t] + dt * mh * (fm(x[t + 1], y[t + 1], m)
+                                                         - (gm(x[t + 1], y[t + 1], m) * fy(x[t + 1], y[t + 1], m)
+                                                            / gy(x[t + 1], y[t + 1], m))));
+
+        yh[t + 1] = -(1. / gy(x[t + 1], y[t + 1], m)) * (xh[t + 1] * gx(x[t + 1], y[t + 1], m)
+                                                        + mh * gm(x[t + 1], y[t + 1], m));
+    }
+}
+
+void Inv_Prb::Inc_Adj(std::vector<double> & x, std::vector<double> & y, double m
+        , std::vector<double> & la, std::vector<double> & mu
+        , std::vector<double> & xh, std::vector<double> & yh, double mh
+        , std::vector<double> & lah, std::vector<double> & muh){
+    lah[la.size()-1] = 0;
+    muh[mu.size()-1] = 0;
+    for (int t = x.size() - 1 ; t >= 0; t--){
+        lah[t - 1] = (1. + dt * fx(x[t], y[t], m)) * lah[t] + dt * (xh[t] + muh[t] * gx(x[t], y[t], m)
+                                                                   + la[t] * (xh[t] * fxx(x[t], y[t], m) +
+                                                                              yh[t] * fxy(x[t], y[t], m)
+                                                                              + mh * fxm(x[t], y[t], m))
+                                                                   + mu[t] * (xh[t] * gxx(x[t], y[t], m) +
+                                                                              yh[t] * gxy(x[t], y[t], m)
+                                                                              + mh * gxm(x[t], y[t], m)));
+
+        muh[t - 1] = -(1. / gy(x[t - 1], y[t - 1], m)) * (lah[t - 1] * fy(x[t - 1], y[t - 1], m)
+                                                         + la[t - 1] * (xh[t - 1] * fyx(x[t - 1], y[t - 1], m) +
+                                                                        yh[t - 1] * fyy(x[t - 1], y[t - 1], m)
+                                                                        + mh * fym(x[t - 1], y[t - 1], m))
+                                                         + mu[t - 1] * (xh[t - 1] * gyx(x[t - 1], y[t - 1], m) +
+                                                                        yh[t - 1] * gyy(x[t - 1], y[t - 1], m)
+                                                                        + mh * gym(x[t - 1], y[t - 1], m)));
+    }
+}
+
+void Inv_Prb::Newton_update(std::vector<double> & x, std::vector<double> & y, double m
+        , std::vector<double> & la, std::vector<double> & mu
+        , std::vector<double> & xh, std::vector<double> & yh
+        , std::vector<double> & lah, std::vector<double> & muh, double W, double Fn){
+    W = 0.;
+    Fn = 0.;
+    for (int t = 0; t < x.size(); t++){
+        W += w(x[t], y[t], m, la[t], mu[t]);
+        Fn += Fnew(x[t], y[t], m, la[t], mu[t], xh[t], yh[t], lah[t], muh[t]);
+    }
+
+    W  *= dt;
+    Fn *= dt;
+}
+
+double Inv_Prb::New_linear_solve(std::vector<double> & x, std::vector<double> & y, double m
+        , std::vector<double> & la, std::vector<double> & mu
+        , std::vector<double> & xh, std::vector<double> & yh
+        , std::vector<double> & lah, std::vector<double> & muh, double grad){
+    double W, Fn;
+    Newton_update(x ,y, m, la, mu, xh, yh, lah, muh, W,Fn);
+
+    return -(Fn + grad)/W;
+}
+
+void Inv_Prb::Observation(std::vector<double> & x, std::vector<double> & d){
+    double noise_lvl = 0.02;
+    double noise_vec;
+
+    for (int i = 0; i < x.size(); i++){
+        noise_vec = noise_lvl * (rand() - 0.5) * 2.;
+        d[i] = noise_vec + x[i];
+    }
+}
+
+void Inv_Prb::Initialize_system(std::vector<double> & x, std::vector<double> & y
+        , std::vector<double> & la, std::vector<double> & mu
+        , std::vector<double> & xh, std::vector<double> & yh
+        , std::vector<double> & lah, std::vector<double> & muh){
+    // forward system
+    x.assign(N, 0.);
+    y.assign(N, 0.);
+    x[0] = 1;
+
+    //adjoint system
+    la.assign(N, 0.);
+    mu.assign(N, 0.);
+    la[N-1] = 0.;
+    mu[N-1] = 0.;
+
+    //Incremental forward system
+    xh.assign(N, 0.);
+    yh.assign(N, 0.);
+    xh[0] = 0.;
+    yh[0] = 0.;
+
+    //Incremental adjoint system
+    lah.assign(N, 0.);
+    muh.assign(N, 0.);
+    lah[N-1] = 0.;
+    muh[N-1] = 0.;
+}
--- DAE_Inverse_Problem_cpp/Inverse_Problem_Fns.h
+++ DAE_Inverse_Problem_cpp/Inverse_Problem_Fns.h
@@ -8,45 +8,62 @@
 #include "Needed_functions.h"
 #include <vector>
 
-// solve the foward problem
-void Forward_solver(std::vector<double> & x, std::vector<double> & y, double m);
-// solve the adjoint problem
-void Adjoint_solver(std::vector<double> & x, std::vector<double> & y, double m
-                    , std::vector<double> & la, std::vector<double> & mu);
+class Inv_Prb{
+private:
+    double dt;
+    int N;
+    double epislon_DAE = 1E-6;
 
-// evaluate the gradient
-void Grad_eval(std::vector<double> & x, std::vector<double> & y, double m, double gnorm
-               , double grad);
-// calculate the cost
-double cost(std::vector<double> & x, std::vector<double> & d);
+    // solve the adjoint problem
+    void Adjoint_solver(std::vector<double> & x, std::vector<double> & y, double m
+            , std::vector<double> & la, std::vector<double> & mu, std::vector<double> & d);
 
-// solve the incremental state problem
-void Inc_state(std::vector<double> & x, std::vector<double> & y, double m
-               , std::vector<double> & xh, std::vector<double> & yh, double mh);
+    // solve the incremental state problem
+    void Inc_state(std::vector<double> & x, std::vector<double> & y, double m
+            , std::vector<double> & xh, std::vector<double> & yh, double mh);
 // solve the incremental adjoint problem
-void Inc_Adj(std::vector<double> & x, std::vector<double> & y, double m
-             , std::vector<double> & la, std::vector<double> & mu
-             , std::vector<double> & xh, std::vector<double> & yh, double mh
-             , std::vector<double> & lah, std::vector<double> & muh);
+    void Inc_Adj(std::vector<double> & x, std::vector<double> & y, double m
+            , std::vector<double> & la, std::vector<double> & mu
+            , std::vector<double> & xh, std::vector<double> & yh, double mh
+            , std::vector<double> & lah, std::vector<double> & muh);
 
-// Create the newton scaler hessian and components that don't include mh
-void Newton_update(std::vector<double> & x, std::vector<double> & y, double m
-                   , std::vector<double> & la, std::vector<double> & mu
-                   , std::vector<double> & xh, std::vector<double> & yh
-                   , std::vector<double> & lah, std::vector<double> & muh, double W, double Fn);
+    // Create the newton scaler hessian and components that don't include mh
+    void Newton_update(std::vector<double> & x, std::vector<double> & y, double m
+            , std::vector<double> & la, std::vector<double> & mu
+            , std::vector<double> & xh, std::vector<double> & yh
+            , std::vector<double> & lah, std::vector<double> & muh, double W, double Fn);
 // solve the newton linear system
-double New_linear_solve(std::vector<double> & x, std::vector<double> & y, double m
-                        , std::vector<double> & la, std::vector<double> & mu
-                        , std::vector<double> & xh, std::vector<double> & yh
-                        , std::vector<double> & lah, std::vector<double> & muh, double grad);
+    double New_linear_solve(std::vector<double> & x, std::vector<double> & y, double m
+            , std::vector<double> & la, std::vector<double> & mu
+            , std::vector<double> & xh, std::vector<double> & yh
+            , std::vector<double> & lah, std::vector<double> & muh, double grad);
 
-// create the observation vector d
-void Observation(std::vector<double> & x, std::vector<double> & d);
 
-// Initialize the vectors need for the inverse problem
-void Initialize_system(int N, std::vector<double> & x, std::vector<double> & y
-                       , std::vector<double> & la, std::vector<double> & mu
-                       , std::vector<double> & xh, std::vector<double> & yh
-                       , std::vector<double> & lah, std::vector<double> & muh);
+public:
+    Inv_Prb();
+    // solve the foward problem
+    void Forward_solver(std::vector<double> & x, std::vector<double> & y, double m);
+
+    // calculate the cost
+    double cost(std::vector<double> & x, std::vector<double> & d);
+
+    // evaluate the gradient
+    void Grad_eval(std::vector<double> & x, std::vector<double> & y, std::vector<double> & la
+                   , std::vector<double> & mu, double m, double gnorm, double grad);
+
+    // create the observation vector d
+    void Observation(std::vector<double> & x, std::vector<double> & d);
+
+    // Initialize the vectors need for the inverse problem
+    void Initialize_system(std::vector<double> & x, std::vector<double> & y
+            , std::vector<double> & la, std::vector<double> & mu
+            , std::vector<double> & xh, std::vector<double> & yh
+            , std::vector<double> & lah, std::vector<double> & muh);
+
+    void assign_dt(double Dt){dt = Dt;};
+    void assign_N(int numb_time_steps) {N = numb_time_steps;};
+
+};
+
 
 #endif //MATH_233_FINAL_PROJECT_INVERSE_PROBLEM_FNS_H

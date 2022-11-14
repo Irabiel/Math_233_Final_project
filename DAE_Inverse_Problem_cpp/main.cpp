0937ca0937a354719f29d4af395ceac3b2bb62a6
Irabiel <irabiel@yahoo.com>
2022-11-14 00:19:16 -0800

    Testing newton method. Ran into segmentation fualt

--- DAE_Inverse_Problem_cpp/main.cpp
+++ DAE_Inverse_Problem_cpp/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 
-#include "Needed_functions.h"
+#include "Inverse_Problem_Fns.h"
 #include "cmath"
+#include <vector>
 int main() {
+    Inv_Prb IP = Inv_Prb();
 
-    std::cout << "Hello, World!" << std::endl;
+    // parameters
+    double t_init         = 0.;
+    double t_final        = 1.;
+    double t_obs          = 1.;
+    double dt             = .01;
+
+    IP.assign_dt(dt);
+
+    int n = round ((t_final - t_init) /dt);
+    IP.assign_N(n);
+
+    // DAE Newton system parameter
+    double epislon_DAE = 1e-6;
+    IP.assign_DAE_tol(epislon_DAE);
+
+    //True parameter
+    double mtrue = 0.693;
+    IP.assign_true_m(mtrue);
+
+    // initial guess
+    double m = 4.609;
+    IP.Newton_Solver(m);
+
     return 0;
 }
